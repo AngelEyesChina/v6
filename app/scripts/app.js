7e731288727066e5289bf36720bee4726a61647b
Hanan <angeleyeschina@gmail.com>
2014-02-11 22:10:24 +0200

    added angular-toggle-switch

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,8 @@ angular.module('practiceLogApp', [
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngRoute'
+    'ngRoute',
+    'toggle-switch'
   ])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
