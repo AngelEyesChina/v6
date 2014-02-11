2550483a3e22775ad6e00a0232b838a2ca499096
Hanan <angeleyeschina@gmail.com>
2014-02-11 22:05:09 +0200

    format code

--- app/scripts/services/exercises.js
+++ app/scripts/services/exercises.js
@@ -3,12 +3,13 @@
 angular.module('practiceLogApp')
   .factory('Exercises', ['$resource', function ($resource) {
     return $resource('/exercises/:exerciseID/',
-      {    }, {
+    {
+    }, {
         /*loan: {
          method: 'PUT',
          params: { bookId: '@bookId' },
          isArray: false
-         }*/
-        /* , method2: { ... } */
-      });
+         }
+    , method2: { ... } */
+    });
   }]);
