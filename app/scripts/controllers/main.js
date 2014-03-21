cdf669624037c60fe7ffafad8cf77c834f148d0f
Hanan <angeleyeschina@gmail.com>
2014-03-21 09:12:10 +0200

    removed "awesomeThings"

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,5 @@
 'use strict';
 
 angular.module('practiceLogApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+  .controller('MainCtrl', function () {
   });
